7ca10444a4a0f40f0dfa1caebd07314223fe6a18
abian <abianoor456@gmail.com>
2021-04-07 23:17:58 +0500

    Countdown page changes

--- app/src/main/java/com/example/sindoq/Countdown.java
+++ app/src/main/java/com/example/sindoq/Countdown.java
@@ -8,32 +8,46 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.BgService;
 import com.example.sindoq.adapter.ConfirmPageAdapter;
 
 public class Countdown extends AppCompatActivity {
-TextView textView;
+TextView textView, text2view2, textview3;
     private int counter=5;
     RecyclerView recyclerView;
     ConfirmPageAdapter confirmPageAdapter;
     CountDownTimer Timer;
+    Button stopbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown);
         textView=findViewById(R.id.countdown);
+        stopbtn= findViewById(R.id.btnstop);
+        text2view2= findViewById(R.id.textView2);
+        textview3= findViewById(R.id.text);
 
         Timer= new CountDownTimer(5000, 1000){
             public void onTick(long millisUntilFinished){
                 textView.setText(String.valueOf(counter));
                 counter--;
+                if(text2view2.getVisibility()!= View.GONE)
+                { text2view2.setVisibility(View.GONE); }
             }
             public  void onFinish(){
+
+                stopbtn.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+                textview3.setVisibility(View.GONE);
+                text2view2.setVisibility(View.VISIBLE);
+                text2view2.setText("In Blocking Service");
                 Intent intent = new Intent();
                 intent.setAction("com.example.sindoq.intent.action.startservice");
                 sendBroadcast(intent);
+
             }
 
         }.start();
